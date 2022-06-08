975cb59c56d8d35f4aacb44d4d32c40832c66a29
yutabee <bsk87m@gmail.com>
2022-06-08 14:57:48 +0900

    アルバムに写真を表示しました

--- album_read.php
+++ album_read.php
@@ -9,6 +9,34 @@
 </head>
 
 <body>
+    <style>
+        body {
+            background: #84fab0;
+            /* Chrome 10-25, Safari 5.1-6 */
+            background: -webkit-linear-gradient(to right, rgba(132, 250, 176, 0.5), rgba(143, 211, 244, 0.5));
+            /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+            background: linear-gradient(to right, rgba(132, 250, 176, 0.5), rgba(143, 211, 244, 0.5))
+        }
+
+        .outPutArea {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .image_card {
+            background-color: white;
+            margin: 20px;
+            padding: 10px;
+            border-radius: 10px;
+            box-shadow: inset black -2px -2px 4px 1px;
+        }
+
+        .image_card>img {
+            border-radius: 10px;
+        }
+    </style>
     <?php
     $className = $_GET['class'];
     // var_dump($className);
@@ -34,14 +62,34 @@
 
     $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    echo '<pre>';
-    var_dump($result);
-    echo '<pre>';
-    exit();
+    // echo '<pre>';
+    // var_dump($result);
+    // echo '<pre>';
+    // exit();
 
     ?>
-    <h1>readページです。</h1>
 
+    <h1><?= $className ?> BOOK</h1>
+    <div id="outPutArea" class="outPutArea"></div>
+    <script>
+        const json_results = <?= json_encode($result) ?>;
+        console.log(json_results);
+        let outText = '';
+        json_results.map((x) => {
+            console.log(x.image_path);
+            let ret = x.image_path.replace('../', '');
+            outText += `<div class="image_card">
+                            <img src="${ret}" style="width:250px;height:200px;">
+                            <div class="image_caption_box">
+                                <p>title: ${x.image_title}</p>
+                                <p>caption: ${x.image_caption}</p>
+                            </div>
+                        </div>`;
+        });
+        console.log(outText);
+        const outPutArea = document.getElementById('outPutArea');
+        outPutArea.innerHTML = outText;
+    </script>
 
 </body>
 
